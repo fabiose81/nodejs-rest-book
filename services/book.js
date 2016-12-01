e729133a1eadc66a7fbe5369f2c05a3d26560085
Fabio Estrela <fabiose@gmail.com>
2016-12-01 13:59:25 -0200

    Add files via upload

--- services/book.js
+++ services/book.js
@@ -4,7 +4,7 @@ var connect = "postgres://postgres:admin@localhost:5432/postgres";
 module.exports.addBook = function(req, res, callback){
     pg.connect(connect, function(err, client, done){
             if(err){
-                callback({status : 500, return : err });
+                callback({error : err});
             }else{
                 client.query('INSERT INTO db2.book(title) values($1) RETURNING id',[req.body.title], function(err, result){
                     if(err){
@@ -21,7 +21,7 @@ module.exports.addBook = function(req, res, callback){
 module.exports.getBooks = function(req, res, callback){
     pg.connect(connect, function(err, client, done){
             if(err){
-                callback({status : 500, return : err });
+                callback({error : err});
             }else{
                 client.query('SELECT id, title FROM db2.book', function(err, result){
                       if(err){
@@ -38,7 +38,7 @@ module.exports.getBooks = function(req, res, callback){
 module.exports.deleteBook = function(_filter, callback){
       pg.connect(connect, function(err, client, done){
            if(err){
-                callback({status : 500, return : err });
+                callback({error : err});
             }else{
                 client.query('DELETE FROM db2.book WHERE id in('+_filter+')', function(err, result){
                     if(err){
